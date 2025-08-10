10fe11356d2173a54c0979f5d6bd6359bca693c2
joseafonsoii <joseantonioafonso5@gmail.com>
2025-08-10 16:30:04 +0100

    Adiciona rota Hello Express

--- myApp.js
+++ myApp.js
@@ -2,13 +2,13 @@ let express = require('express');
 let app = express();
 
 
-console.log("Hello World");
-// Adicione essa linha:
-//app.get("/", function(req, res) {
-//  res.send("Hello Express");
-//});
 
-//module.exports = app;
+// Adicione essa linha:
+app.get("/", function(req, res) {
+ res.send("Hello Express");
+});
+
+module.exports = app;
 
 
 
