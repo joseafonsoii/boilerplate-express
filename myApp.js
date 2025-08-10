06a3dbaa5c11e1433d8076823c9efd1c65bf19ae
joseafonsoii <joseantonioafonso5@gmail.com>
2025-08-10 17:15:45 +0100

    Implement a Root-Level Request Logger Middleware 2

--- myApp.js
+++ myApp.js
@@ -1,44 +1,18 @@
 require('dotenv').config();
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
-// Comente ou remova a rota anterior
-// app.get("/", function(req, res) {
-//   res.send("Hello Express");
-// });
-
-// Envie o arquivo index.html
-//app.get("/", function(req, res) {
-//  const absolutePath = __dirname + "/views/index.html";
-//  res.sendFile(absolutePath);
-//});
-
-// Middleware para servir arquivos estáticos
-//app.use("/public", express.static(__dirname + "/public"));
-
-// Rota JSON
-//app.get("/json", function(req, res) {
-//  res.json({ message: "Hello json" });
-//});
-
-//app.get("/json", function(req, res) {
-// let message = "Hello json";
-
- // if (process.env.MESSAGE_STYLE === "uppercase") {
- //   message = message.toUpperCase();
- // }
-
- // res.json({ message: message });
-//});
-
-// Middleware de log
-app.use(function(req, res, next) {
+// Root level logger middleware (tem que estar aqui no topo)
+app.use(function (req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
 });
 
-// Exemplo de rota
-app.get("/json", function(req, res) {
+// Middleware para arquivos estáticos
+app.use("/public", express.static(__dirname + "/public"));
+
+// Rota /json
+app.get("/json", (req, res) => {
   let message = "Hello json";
   if (process.env.MESSAGE_STYLE === "uppercase") {
     message = message.toUpperCase();
@@ -46,4 +20,4 @@ app.get("/json", function(req, res) {
   res.json({ message: message });
 });
 
-
+module.exports = app;
